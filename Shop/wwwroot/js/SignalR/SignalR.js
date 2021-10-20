ed57310d5f54b880daf03ed969b4742c5d702f82
Сергей Шевченко <Serwlashev@gmail.com>
2021-10-20 08:19:07 +0300

    Added wwwroot folder to git

---
+++ Shop/wwwroot/js/SignalR/SignalR.js
@@ -0,0 +1,31 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("http://localhost:5000//cartHub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+$("button").click(() => {
+    let message = $("#txtMessage").val();
+    connection.invoke("SendMessageAsync", message)
+        .catch(error => console.log("Mesaj gönderilirken hata alınmıştır."));
+});
+
+connection.on("receiveMessage", message => {
+    $("#messages").append(`${message}<br>`);
+});
+
+connection.onclose(async () => {
+    await start();
+});
+
+// Start the connection.
+start();
---
+++ Shop/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function AddProduct() {
+
+    $.ajax({
+        type: "GET",
+        url: "/Products/AddToCart",
+        success: function (response) {
+            alert("Hello: " + response.Name + " .\nCurrent Date and Time: " + response.DateTime);
+        },
+        failure: function (response) {
+            alert(response.responseText);
+        },
+        error: function (response) {
+            alert(response.responseText);
+        }
+    })
+}
